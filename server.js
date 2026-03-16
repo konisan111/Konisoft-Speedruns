b15b0e71fc38f6c8d8ef19ae28ae97fb4140b95f
Koni <137056695+konisan1111@users.noreply.github.com>
2026-03-16 09:48:36 +0100

    A nemzetiség is kötelező adat.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true }, // Email address of the user
     password: { type: String, required: true }, // Password for user
     avatarUrl: { type: String }, // R2 address link
-    nationality: { type: String },
+    nationality: { type: String, required: true  },
     userId: { type: String, unique: true } // UUID
 });
 
