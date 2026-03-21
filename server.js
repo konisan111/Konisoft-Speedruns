490c927f58ee0fee9d617976b0d25b53c5cc6142
Koni <137056695+konisan1111@users.noreply.github.com>
2026-03-21 15:10:51 +0100

    S3CLIENT kétszer lett defelve...

--- server.js
+++ server.js
@@ -65,16 +65,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB!'))
   .catch(err => console.error('MongoDB error:', err));
 
-// Cloudflare R2 connection
-const s3 = new S3Client({
-  region: 'auto',
-  endpoint: process.env.R2_ENDPOINT,
-  credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY,
-    secretAccessKey: process.env.R2_SECRET_KEY,
-  },
-});
-
 app.get('/', (req, res) => {
   res.send(`
     <!DOCTYPE html>
