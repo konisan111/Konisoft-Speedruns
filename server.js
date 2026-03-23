c3cf3978e1a743457a408bb30e6fa70db050adfa
Koni <137056695+konisan1111@users.noreply.github.com>
2026-03-23 11:43:13 +0100

    PORT defined hiba javítása.

--- server.js
+++ server.js
@@ -230,7 +230,6 @@ mongoose.connect(process.env.MONGO_URI)
     `);
 });
   
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port \${PORT}`);
 });
